a83488fe22c10d08214b7b001e835fd733637708
TheNewCivilian <git@prenninger.de>
2024-12-27 21:16:40 +0100

    Adding ivr logic

--- ivr/main.py
+++ ivr/main.py
@@ -4,114 +4,130 @@ import asyncio
 import logging
 import os
 import re
+import json
 
-from yate.protocol import MessageRequest
 from yate.ivr import YateIVR
+
+import datetime
+import pickledb
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='./call.log', encoding='utf-8', level=logging.DEBUG)
 
 LOG_FILE = "/tmp/call.log"
 
 SOUNDS_PATH = "./ivr/audio/"
 
-async def play_error(ivr: YateIVR, error_code: int):
-    error_code = str(error_code).zfill(4)
+def log_call(ivr: YateIVR, event: str):
+    with open("calllog.csv", "a") as call_log:
+        time_str = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+        call_log.write("{},{}, {}\n".format(ivr.call_params.get("caller"), time_str, event))
+
+async def handle_error_code(ivr: YateIVR, lang: str):
+    await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/enter_code_{}.sln".format(lang)))
+    plz = await ivr.read_dtmf_symbols(4)
+    error_code = str(plz).zfill(4)
+    callername = "e" + error_code + "/" + ivr.call_params.get("called") + "/" + ivr.call_params.get("callername")
     error_file = os.path.join(SOUNDS_PATH, "errors", error_code + ".sln")
-    await ivr.play_soundfile(error_file, complete=True)
+    log_call(ivr, "Error code: " + error_code)
+    if not os.path.isfile(error_file):
+        await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/error_code_does_not_exist_{}.sln".format(lang)), complete=True)
+    else:
+        await ivr.play_soundfile(error_file, complete=True)
+        await asyncio.sleep(1)
+        await ivr.play_soundfile(error_file, complete=True)
+        await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/error_code_agent_{}.sln".format(lang)), complete=True)  
+    await call_operators(ivr, callername)
 
 async def main(ivr: YateIVR):
     caller_id = ivr.call_params.get("caller", "")
     caller_id = re.sub("[^\\d]", "", caller_id)
     caller_id = re.sub("^\\+", "00", caller_id)
 
+    if (ivr.call_params.get("called") == "8787"):
+        await hotline(ivr)
+    elif (ivr.call_params.get("called") == "7372"):
+         await admin_setup(ivr)
+
+async def admin_setup(ivr: YateIVR):
+    await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/admin_please_enter_password.sln"))
+    password_input = await ivr.read_dtmf_symbols(5)
+    if password_input != "23646":
+        return
+    
+    # Play enter sound
+    await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/admin_hello.sln"))
+    # Set state
+    state_input = await ivr.read_dtmf_symbols(1)
+
+    db = pickledb.load('./rustations.db', False)
+    data = db.get('admins')
+    if data == None or not data:
+        db.set('admins', json.dumps([]))
+        data = db.get('admins')
+    admins = json.loads(db.get('admins'))
+
+    sip_uri = ivr.call_params.get("sip_from")
+    if state_input == "1":
+        # Check if caller id is in operator list else add it
+        if admins == None or len(admins) == 0:
+            admins = [sip_uri]
+        elif sip_uri not in admins:
+            admins.append(sip_uri)
+        db.set('admins', json.dumps(admins))
+        db.dump()
+        await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/admin_sign_in.sln"), complete=True)
+    elif state_input == "2":
+        # Remove caller id from operator list
+        if admins != None and sip_uri in admins:
+            admins.remove(sip_uri)
+            db.set('admins', json.dumps(admins))
+        db.dump()
+        await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/admin_sign_out.sln"), complete=True)
+    elif state_input == "3":
+        await ivr.ring_extension("sip:8631@voip.eventphone.de")
+        # await ivr.fork_call(["sip:8631@voip.eventphone.de"], "admin")
+
+async def call_operators(ivr: YateIVR, callername: str):
+    log_call(ivr, "Call operators")
+    db = pickledb.load('./rustations.db', False)
+    data = db.get('admins')
+    if data == None or not data:
+        return
+    admins = json.loads(data)
+    if len(admins) == 0:
+        await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/not_staffed_one_hour.sln"), complete=True)
+        return
+    await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/waiting_announcement_1_en.sln"), complete=True)
+    await ivr.fork_call(admins, callername)
+
+async def hotline(ivr:YateIVR):
     # Welcome to the rust hotline, for german press 1, for english press 2
     await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/intro.sln"))
+    log_call(ivr, "Call enter")
 
     lang = "de"
     
-    plz = await ivr.read_dtmf_symbols(1)
-
-    print("input", plz)
-    # while len(plz) == 0:
-    #     plz = await ivr.read_dtmf_symbols(1)
+    plz = await ivr.read_dtmf_symbols(1, 12)
 
     if plz == "1":
         lang = "de"
     elif plz == "2":
         lang = "en"
-
+    elif plz == "":
+        callername = ivr.call_params.get("caller") + "/" + ivr.call_params.get("callername")
+        await call_operators(ivr, callername)
     # For general questions 1, compiler errors 2, rewrite in rust 3, unsafe rust 4
     await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/main_menu_{}.sln".format(lang)))
 
+    plz = await ivr.read_dtmf_symbols(1, 23)
 
-    plz = await ivr.read_dtmf_symbols(1)
-
-    if plz == "1":
-        return
+    if plz == "" or plz == "1" or plz == "3" or plz == "4":
+        callername = lang + "/"+ ivr.call_params.get("caller") + "/" + ivr.call_params.get("callername")
+        await call_operators(ivr, callername)
     elif plz == "2":
-        await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/enter_code_{}.sln".format(lang)))
-        plz = await ivr.read_dtmf_symbols(4)
-        await play_error(ivr, int(plz))
-        await asyncio.sleep(1)
-        await play_error(ivr, int(plz))
-        await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "messages/error_code_agent_{}.sln".format(lang)), complete=True)
+        await handle_error_code(ivr, lang)
 
-
-
-    # await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "ansage_1_intro_plz.slin"))
-    # plz = await ivr.read_dtmf_symbols(5)
-    # if len(plz) != 5:
-    #     plz = "00000"
-    # await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "ansage_2_intro_hilfe.slin"), complete=True)
-
-    # for _ in range(3):
-    #     await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "ansage_3_auswahl_der_hilfe.slin"))
-    #     help_id = await ivr.read_dtmf_symbols(1, 34)
-    #     if help_id in ["1", "2", "3", "4", "5"]:
-    #         break
-    # if help_id == "":
-    #     help_id = "5"
-
-    # if help_id == "5":
-    #     call_msg = MessageRequest("chan.masquerade", {
-    #         "message": "call.execute",
-    #         "id": ivr.call_id,
-    #         "callto": FORWARD_PHONE_ADDRESS,
-    #         "line": LINE,
-    #         "oconnection_id": "external_udp",
-    #         "osip_From": SIP_FROM_HEADER,
-    #     })
-    #     result = await ivr.send_message_async(call_msg)
-    #     if not result.processed:
-    #         await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "ansage_sibernetz_fail.slin"), complete=True)
-    # else:
-    #     query = {
-    #         "token": API_TOKEN,
-    #         "phone": caller_id,
-    #         "zip": plz,
-    #         "request": help_id,
-    #     }
-    #     success = True
-    #     error_message = ""
-    #     try:
-    #         api_result = requests.get(URL, query)
-    #     except requests.exceptions.RequestException as e:
-    #         success = False
-    #         error_message = str(e)
-
-    #     if success and api_result.status_code != 200:
-    #         success = False
-    #         error_message = "HTTP error: " + str(api_result.status_code)
-
-    #     if not success:
-    #         fallback_filename = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-") + uuid.uuid4().hex + "response.sbj"
-    #         fallback_file = os.path.join(FALLBACK_FILES_DIR, fallback_filename)
-    #         with open(fallback_file, "w") as f:
-    #             f.write(error_message + "\n")
-    #             f.write(repr(query))
-    #     else:
-    #         await ivr.play_soundfile(os.path.join(SOUNDS_PATH, "ansage_4_vielen_dank.slin"), complete=True)
-    #         await asyncio.sleep(0.5)
-
-
-logging.basicConfig(filename=LOG_FILE, filemode="a+")
 app = YateIVR()
 app.run(main)
